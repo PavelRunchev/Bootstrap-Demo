91cd402220c75bab993b971e16e6250ec9999b9b
PavelRunchev <pavel.runchev@gmail.com>
2018-12-20 20:05:02 +0200

    added new pages - Dropify and Sign Up.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 
+
+// Sweet Alert Plugin
 $('#clickedSuccess').click(function () {
     swal("Good job!", "You clicked the button!", "success");
 });
@@ -36,3 +38,46 @@ $('#clickedDeleted').click(function () {
         swal("Deleted!", "Your imaginary file has been deleted.", "success");
       });
 });
+
+// Register User
+$('#submit').click(register);
+
+function register(e) {
+  e.preventDefault();
+  swal("Good job!", "You are registered!", "success");
+  let firstName = $('#firstName').val();
+  let lastName = $('#lastName').val();
+  let email = $('#email').val();
+  let newUser = `${firstName} ${lastName} - ${email}`;
+
+  let users = $('.list-group');
+  let newList = $('<li>')
+  newList.addClass('list-group-item');
+  newList.text(newUser);
+
+  let closeBtn = $('<a>X</a>')
+    .addClass('btn btn-danger float-right closeBtn p-1 text-light').click(removeUser);
+
+  newList.append(closeBtn);
+  users.append(newList);
+}
+
+$('.closeBtn').click(removeUser);
+
+
+function removeUser() {
+    let currentUser = $(this).parent();
+    swal({
+      title: "Are you sure?",
+      text: "You will not be to recover after!",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonClass: "btn-danger",
+      confirmButtonText: "Yes, delete it!",
+      closeOnConfirm: false
+    },
+    function(){
+      swal("Deleted!", "Curent user has been deleted!", "success");
+      currentUser.remove();
+    });
+};
